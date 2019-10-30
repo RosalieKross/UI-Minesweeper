d80fc0bc4b2fca5e3cf7f40bf9ffefdaf8d7cf53
RosalieKross <50362912+RosalieKross@users.noreply.github.com>
2019-10-30 20:02:42 +0100

    UI

---
+++ UI_MineSweeper/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    public Image progressBar;
+    public float endScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckScore();
+    }
+
+    public void CheckScore()
+    {
+        //progressBar.fillAmount = //hier komt je score value bv: currentScore / endScore; Als endScore 100 is en current score op 25 dan zal de bar voor een 4e gevuld zijn
+    }
+}
---
+++ UI_MineSweeper/Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    //public GameObject tapToHoldpanel;
+    //public GameObject homeButton;
+    //public GameObject LevelPanel;
+
+    public void BeginGame()
+    {
+        //tapToHoldpanel.SetActive(false);
+        //homeButton.SetActive(true);
+        SceneManager.LoadScene("Level1");
+    }
+}
